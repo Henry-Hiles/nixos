4613bc2e9b08bc7f04cceac59cb884eb316f4b9c
Henry Hiles <henry@henryhiles.com>
2023-10-29 17:33:08 -0400

    Add sway

---
+++ home-manager/sway.nix
@@ -0,0 +1,12 @@
+wayland.windowManager.sway = {
+  enable = true;
+  config = rec {
+    modifier = "Mod4";
+    # Use kitty as default terminal
+    terminal = "kitty"; 
+    startup = [
+      # Launch Firefox on start
+      {command = "firefox";}
+    ];
+  };
+};
