5f9c39b5ff354a66c2b58ff1b043012dac2b2b98
Henry-Hiles <henry@henryhiles.com>
2024-12-11 14:08:35 -0500

    Enable bluetooth, fix wheel err

---
+++ modules/common-desktop/bluetooth.nix
@@ -0,0 +1,1 @@
+{lib, ...}: {hardware.bluetooth.enable = lib.mkForce true;}
--- modules/common-desktop/wheel.nix
+++ modules/common-desktop/wheel.nix
@@ -1,17 +1,6 @@
 {pkgs, ...}: {
-  boot.extraModulePackages = [
-    (pkgs.linuxKernel.packages.linux_zen.hid-tmff2.overrideAttrs
-      (prev: {
-        src = pkgs.fetchFromGitHub {
-          owner = "Kimplul";
-          repo = "hid-tmff2";
-          rev = "cc4226299569b5a1402c0b937c6b3a2e0f246af4";
-          hash = "sha256-Hv6eLbf5K9qGhweKHHf7IpZaCFsLMCvC0vxBapQOSpQ=";
-          fetchSubmodules = true;
-        };
-      }))
-  ];
+  # boot.extraModulePackages = [pkgs.linuxKernel.packages.linux_zen.hid-tmff2];
 
   environment.systemPackages = with pkgs; [oversteer linuxConsoleTools];
-  services.udev.packages = [pkgs.oversteer];
+  # services.udev.packages = [pkgs.oversteer];
 }
