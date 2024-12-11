5861726feb6394c9178b857b95fd444100d32450
Henry-Hiles <henry@henryhiles.com>
2024-12-10 19:56:33 -0500

    rm retroarch

--- modules/common-desktop/packages.nix
+++ modules/common-desktop/packages.nix
@@ -31,6 +31,5 @@
     nodePackages_latest.pnpm
     hunspellDicts.en_CA-large
     inputs.nix-gaming.packages.${system}.wine-ge
-    (retroarch.override {cores = with libretro; [bsnes-hd];})
   ];
 }
