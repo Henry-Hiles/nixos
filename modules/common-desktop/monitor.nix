777f8906acf9d248fc8090fc970490d96befc36e
Henry-Hiles <henry@henryhiles.com>
2024-12-31 12:28:10 -0500

    Fix shutdown hook

--- modules/common-desktop/monitor.nix
+++ modules/common-desktop/monitor.nix
@@ -2,18 +2,21 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  setvcp = "${lib.meta.getExe pkgs.ddcutil} setvcp D6";
+in {
   hardware.i2c.enable = true;
   systemd.services.monitor-off = rec {
-    script = "${lib.meta.getExe pkgs.ddcutil} setvcp D6 05";
-    wantedBy = ["sleep.target" "poweroff.target"];
+    script = "${setvcp} 05";
+    wantedBy = ["sleep.target" "final.target"];
     before = wantedBy;
 
-    serviceConfig = {Type = "oneshot";};
+    serviceConfig.Type = "oneshot";
+    unitConfig.DefaultDependencies = false;
   };
 
   systemd.services.monitor-on = rec {
-    script = "${lib.meta.getExe pkgs.ddcutil} setvcp D6 01";
+    script = "${setvcp} 01";
     wantedBy = ["sleep.target" "multi-user.target"];
     after = wantedBy;
   };
