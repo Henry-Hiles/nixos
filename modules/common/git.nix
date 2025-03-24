f1b2f6853afa6afae5afde846e0ffeaa0ce02829
Henry-Hiles <henry@henryhiles.com>
2025-03-24 13:54:05 -0400

    git signing

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -1,9 +1,14 @@
 {pkgs, ...}: {
   programs.git = {
     enable = true;
-    config.user = {
-      name = "Henry-Hiles";
-      email = "henry@henryhiles.com";
+    config = {
+      user = {
+        name = "Henry-Hiles";
+        email = "henry@henryhiles.com";
+        signingKey = builtins.elemAt (import ../../secrets/keys.nix) 0;
+      };
+      commit.gpgsign = true;
+      gpg.format = "ssh";
     };
   };
 
