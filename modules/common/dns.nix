61bf7ec05368b8cb8c8f0818ca638de8636bf348
Henry-Hiles <henry@henryhiles.com>
2025-03-23 20:20:49 -0400

    Fix dns

--- modules/common/dns.nix
+++ modules/common/dns.nix
@@ -3,10 +3,11 @@
     enable = true;
     settings = rec {
       upstream = ["https://base.dns.mullvad.net/dns-query"];
+      listen-addrs = ["127.0.0.1"];
       fallback = ["1.1.1.1"];
       bootstrap = fallback;
     };
   };
 
-  environment.etc."resolv.conf".text = "nameserver 0.0.0.0";
+  environment.etc."resolv.conf".text = "nameserver 127.0.0.1";
 }
