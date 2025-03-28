36d82909ec53674614218a0588e53ecf76103fe2
Henry-Hiles <henry@henryhiles.com>
2025-03-27 21:11:43 -0400

    QMK Config

--- modules/desktop/qmk.nix
+++ modules/desktop/qmk.nix
@@ -1,4 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   hardware.keyboard.qmk.enable = true;
-  environment.systemPackages = [pkgs.qmk];
+  environment.systemPackages = [
+    (pkgs.qmk.overrideAttrs (oldAttrs: {
+      propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ [pkgs.python3.pkgs.appdirs];
+    }))
+  ];
+
+  systemd.tmpfiles.settings.qmk = {
+    "/home/quadradical/.config/qmk"."d".user = "quadradical";
+    "/home/quadradical/.config/qmk/qmk.ini"."f+".argument = lib.replaceStrings ["\n"] ["\\n"] (lib.generators.toINI {} {
+      user = {
+        qmk_home = "/home/quadradical/Documents/Code/qmk_firmware";
+        overlay_dir = "/home/quadradical/Documents/Code/qmk_userspace";
+        keyboard = "keychron/v1_max/ansi_encoder";
+        keymap = "default";
+      };
+    });
+  };
 }
