cbf21eba0dd998853780535007fee2dd2af3148b
Henry-Hiles <henry@henryhiles.com>
2025-03-31 22:14:40 -0400

    Working grapevine

--- clients/quadraticserver/matrix.nix
+++ clients/quadraticserver/matrix.nix
@@ -2,26 +2,6 @@
   imports = [inputs.grapevine.nixosModules.default];
   networking.firewall.allowedTCPPorts = [8448];
 
-  systemd.tmpfiles.rules = [
-    "d /var/lib/private/matrix-conduit 0770 conduit conduit"
-    "d /var/lib/private/matrix-conduit/database 0770 conduit conduit"
-    "d /var/lib/private/matrix-conduit/media 0770 conduit conduit"
-    "L /var/lib/matrix-conduit /var/lib/private/matrix-conduit"
-  ];
-
-  users = {
-    groups.conduit = {};
-    users.conduit = {
-      isSystemUser = true;
-      group = "conduit";
-    };
-  };
-
-  systemd.services.grapevine.serviceConfig = {
-    User = "conduit";
-    Group = "conduit";
-  };
-
   services = let
     domain = "matrix.henryhiles.com";
     # socket = "/run/grapvine/socket";
@@ -29,16 +9,11 @@
     grapevine = {
       enable = true;
       settings = {
-        server_name = domain;
-        conduit_compat = true;
+        server_name = "henryhiles.com";
         database.backend = "rocksdb";
-
-        allow_registration = true;
-        registration_token = "test";
 
         federation = {
           max_concurrent_requests = 10000;
-          self_test = false;
         };
 
         server_discovery = {
@@ -57,7 +32,7 @@
 
     caddy.virtualHosts."${domain}" = {
       serverAliases = ["${domain}:8448"];
-      extraConfig = "reverse_proxy 127.0.0.3";
+      extraConfig = "reverse_proxy 127.0.0.3:6167";
     };
   };
 }
