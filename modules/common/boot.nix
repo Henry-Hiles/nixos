30f94ad28874b3562d924b4760831b994f58de39
Henry-Hiles <henry@henryhiles.com>
2025-05-04 21:16:02 -0400

    RM Zen

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -1,10 +1,9 @@
-{pkgs, ...}: {
+{
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_zen;
     kernelParams = ["sysrq_always_enabled=1"];
   };
 }
