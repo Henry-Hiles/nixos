8d596d5ed42c1f6ed91952f0fb877acb492a9a82
Henry-Hiles <henry@henryhiles.com>
2025-05-10 17:31:34 -0400

    Use ssh

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -7,6 +7,11 @@
         email = "henry@henryhiles.com";
         signingKey = builtins.elemAt (import ../../secrets/keys.nix) 0;
       };
+      url = {
+        "git@github.com:".insteadOf = ["https://github.com/"];
+        "git@git.henryhiles.com:".insteadOf = ["https://git.henryhiles.com"];
+      };
+      init.defaultBranch = "main";
       commit.gpgsign = true;
       pull.rebase = true;
       gpg.format = "ssh";
