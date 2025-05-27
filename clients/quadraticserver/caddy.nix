563b4debe72793aa3c6ef69c7abd64b9d2a06a5d
Henry-Hiles <henry@henryhiles.com>
2025-05-27 15:03:52 -0400

    Fix element call, more librewolf prefs

--- clients/quadraticserver/caddy.nix
+++ clients/quadraticserver/caddy.nix
@@ -3,5 +3,5 @@
     enable = true;
     email = "henry@henryhiles.com";
   };
-  networking.firewall.allowedTCPPorts = [2222 443];
+  networking.firewall.allowedTCPPorts = [2222 443 8448]; # SSH,Matrix and HTTPS
 }
--- modules/desktop/librewolf/default.nix
+++ modules/desktop/librewolf/default.nix
@@ -34,9 +34,12 @@
     package = pkgs.librewolf;
 
     autoConfig = lib.concatStringsSep "\n" (lib.mapAttrsToList (pref: value: "lockPref(\"${pref}\", ${builtins.toJSON value});") {
-      "svg.context-properties.content.enabled" = true;
+      "browser.discovery.containers.enabled" = false;
+      "privacy.resistFingerprinting" = false;
+      "privacy.fingerprintingProtection" = true;
       "privacy.fingerprintingProtection.overrides" = "+AllTargets,-CSSPrefersColorScheme";
       "media.peerconnection.enabled" = true;
+      "svg.context-properties.content.enabled" = true;
       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       "browser.uiCustomization.state" = "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\"],\"currentVersion\":19}";
     });
