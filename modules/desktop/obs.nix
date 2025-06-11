bb458da5a82e50d97964e6cafea267ed7681ff1b
Henry-Hiles <henry@henryhiles.com>
2025-06-11 12:16:11 -0400

    add plugin

--- modules/desktop/obs.nix
+++ modules/desktop/obs.nix
@@ -1,1 +1,6 @@
-{programs.obs-studio.enable = true;}
+{pkgs, ...}: {
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [obs-pipewire-audio-capture];
+  };
+}
