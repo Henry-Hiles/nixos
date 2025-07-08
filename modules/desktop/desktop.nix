c81c5df0ea830766396fbb8803a79f0e1e30d93b
Henry-Hiles <henry@henryhiles.com>
2025-07-08 10:40:13 -0400

    Use portals (I shouldn't really do this)

--- modules/desktop/desktop.nix
+++ modules/desktop/desktop.nix
@@ -1,5 +1,8 @@
 {
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    GTK_USE_PORTAL = "1";
+  };
 
   services = {
     desktopManager.gnome.enable = true;
