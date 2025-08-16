16ce78ace5036a60fba3bb1b4f82a72cff5cfdba
Henry-Hiles <henry@henryhiles.com>
2025-08-16 13:32:00 -0400

    remove default fonts

--- modules/graphical/fonts.nix
+++ modules/graphical/fonts.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   fonts = {
     packages = with pkgs; [inter nerd-fonts.fira-code twitter-color-emoji];
+    enableDefaultPackages = false;
     fontconfig.defaultFonts = rec {
       serif = ["Inter"];
       sansSerif = serif;
