308330af2c297596cbfde6f05f36057f4a0f37b4
Henry-Hiles <henry@henryhiles.com>
2025-08-29 13:43:06 -0400

    remove un-needed overlays

--- clients/quadphone/overlays.nix
+++ clients/quadphone/overlays.nix
@@ -12,26 +12,11 @@ in {
 
   nixpkgs.overlays = [
     (_: super: {
-      ruby_3_3 = super.ruby_3_3.overrideAttrs (old: {
-        NIX_RUSTFLAGS =
-          (old.NIX_RUSTFLAGS or "")
-          + " --target ${super.stdenv.hostPlatform.rust.rustcTargetSpec}";
-      });
-
       # https://github.com/NixOS/nixpkgs/pull/434550
       iniparser = super.iniparser.overrideAttrs (old: {
         cmakeFlags = [
           (lib.cmakeBool "BUILD_TESTING" false)
         ];
-      });
-
-      # https://github.com/NixOS/nixpkgs/pull/434579
-      gnome-user-share = super.gnome-user-share.overrideAttrs (old: {
-        env.CARGO_BUILD_TARGET = super.stdenv.hostPlatform.rust.rustcTargetSpec;
-        postPatch = ''
-          substituteInPlace src/meson.build \
-            --replace-fail "'cp', 'src' / rust_target / meson.project_name(), '@OUTPUT@'," "'cp', 'src' / '${super.stdenv.hostPlatform.rust.cargoShortTarget}' / rust_target / meson.project_name(), '@OUTPUT@',"
-        '';
       });
 
       # TODO: Upstream to nixpkgs
@@ -46,22 +31,12 @@ in {
         cargoVendorDir = "vendor";
       });
 
-      # https://github.com/NixOS/nixpkgs/pull/434998
-      ibus = super.ibus.overrideAttrs (old: {
-        buildInputs = (old.buildInputs or []) ++ [super.wayland-scanner];
+      # TODO: Maybe upstream to nixpkgs (ruby maintainer please reply to my DM)
+      ruby_3_3 = super.ruby_3_3.overrideAttrs (old: {
+        NIX_RUSTFLAGS =
+          (old.NIX_RUSTFLAGS or "")
+          + " --target ${super.stdenv.hostPlatform.rust.rustcTargetSpec}";
       });
-
-      # https://github.com/NixOS/nixpkgs/pull/431159
-      gst_all_1 =
-        super.gst_all_1
-        // {
-          gst-editing-services = super.gst_all_1.gst-editing-services.overrideAttrs (old: {
-            mesonFlags = [
-              (lib.mesonEnable "doc" false)
-              (lib.mesonEnable "tests" false)
-            ];
-          });
-        };
     })
   ];
 }
