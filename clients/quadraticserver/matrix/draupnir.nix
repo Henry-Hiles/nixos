ede7de10a0a37ba00c39ad4b2e2acb581df7965d
Henry-Hiles <henry@henryhiles.com>
2025-10-04 11:25:00 -0400

    bump

--- clients/quadraticserver/matrix/draupnir.nix
+++ clients/quadraticserver/matrix/draupnir.nix
@@ -2,29 +2,34 @@
 {
   services.draupnir = {
     enable = true;
-    settings = rec {
-      homeserverUrl = config.services.matrix-continuwuity.settings.global.well_known.client;
-      rawHomeserverUrl = homeserverUrl;
+    settings =
+      let
+        serverName = config.services.matrix-continuwuity.settings.global.server_name;
+        homeserverUrl = config.services.matrix-continuwuity.settings.global.well_known.client;
+      in
+      {
+        inherit homeserverUrl;
+        rawHomeserverUrl = homeserverUrl;
 
-      managementRoom = "#moderators:${config.services.matrix-continuwuity.settings.global.server_name}";
+        managementRoom = "#moderators:${serverName}";
 
-      autojoinOnlyIfManager = false;
-      protectAllJoinedRooms = true;
+        autojoinOnlyIfManager = false;
+        acceptInvitesFromSpace = "#space:${serverName}";
+        protectAllJoinedRooms = true;
 
-      roomStateBackingStore.enabled = false;
+        roomStateBackingStore.enabled = false;
 
-      commands = {
-        allowNoPrefix = true;
-        symbolPrefixes = [ "/" ];
-        ban.defaultReasons = [
-          "spam"
-          "advertising"
-          "harassment"
-          "troll"
-        ];
+        commands = {
+          allowNoPrefix = true;
+          ban.defaultReasons = [
+            "spam"
+            "advertising"
+            "harassment"
+            "troll"
+          ];
+        };
+
       };
-
-    };
     secrets.accessToken = config.age.secrets."draupnir.age".path;
   };
 }
