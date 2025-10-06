882efd316b4ab1e6b246eacb67ada81bbf4303bb
Henry-Hiles <henry@henryhiles.com>
2025-10-06 14:45:38 -0400

    use cinny fork

--- clients/quadraticserver/matrix/cinny.nix
+++ clients/quadraticserver/matrix/cinny.nix
@@ -8,19 +8,19 @@
           homeserverList = [ "federated.nexus" ];
           allowCustomHomeservers = false;
         };
-        # cinny-unwrapped = pkgs.cinny-unwrapped.overrideAttrs (old: rec {
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "GigiaJ";
-        #     repo = "cinny";
-        #     rev = "a299e9c4cb4df1a3b732fdfddb1297170251a10d";
-        #     hash = "sha256-EDsDVOlaYT0S30Cml+t71U7OOKkfcE4aJxwE8iTdV3s=";
-        #   };
-        #   npmDeps = pkgs.fetchNpmDeps {
-        #     inherit src;
-        #     name = "${old.pname}-${old.version}-npm-deps";
-        #     hash = "sha256-k8eCQO1uIpoKpLHO3E3EYWbQSjcAya2AxngA9mvSfns=";
-        #   };
-        # });
+        cinny-unwrapped = pkgs.cinny-unwrapped.overrideAttrs (old: rec {
+          src = pkgs.fetchFromGitHub {
+            owner = "gingershaped";
+            repo = "cinny";
+            rev = "13dd8fcc0696d18e2af0f8eed234ff64660f402e";
+            hash = "sha256-AHKiqXmL5J7Yt8hxjdgJJQwOYwKwSyDNfJxvuyMG13s=";
+          };
+          npmDeps = pkgs.fetchNpmDeps {
+            inherit src;
+            name = "${old.pname}-${old.version}-npm-deps";
+            hash = "sha256-lBt42rKqNpdpfQt0YlZoxFa9W+1LbY33Uitknsizrng=";
+          };
+        });
       }
     }
     try_files {path} {path}/ /index.html
