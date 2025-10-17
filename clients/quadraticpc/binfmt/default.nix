3b6b38509614cfcb4adeb294a0b7268c60877377
Henry-Hiles <henry@henryhiles.com>
2025-10-17 14:04:07 -0400

    disable qemu patches for now

--- clients/quadraticpc/binfmt/default.nix
+++ clients/quadraticpc/binfmt/default.nix
@@ -1,11 +1,11 @@
 {
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  nixpkgs.overlays = [
-    (_: super: {
-      # Because of https://github.com/NixOS/nixpkgs/pull/378579
-      qemu = super.qemu.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [ ./qemu.patch ];
-      });
-    })
-  ];
+  # boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  # nixpkgs.overlays = [
+  #   (_: super: {
+  #     # Because of https://github.com/NixOS/nixpkgs/pull/378579
+  #     qemu = super.qemu.overrideAttrs (old: {
+  #       patches = (old.patches or [ ]) ++ [ ./qemu.patch ];
+  #     });
+  #   })
+  # ];
 }
