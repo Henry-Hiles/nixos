9fadebd56ef25afaa0e2968f59cf49e71b0aa728
Henry-Hiles <henry@henryhiles.com>
2025-10-17 13:59:19 -0400

    add rebuild-nova to nh.nix, bump

--- modules/common/nh.nix
+++ modules/common/nh.nix
@@ -5,11 +5,16 @@
     flake = "/home/quadradical/.config/nixos";
   };
 
-  environment.shellAliases = {
-    clean = "nh clean all";
-    update = "env -C ~/.config/nixos nix flake update";
-    upgrade = "nh os switch --update";
-    rebuild = "nh os switch";
-    rebuild-server = "nh os switch --hostname quadraticserver --target-host 192.168.0.132";
-  };
+  environment.shellAliases =
+    let
+      build = "nixos-rebuild switch --flake ~/.config/nixos#quadraticserver server --ask-sudo-password --target-host ";
+    in
+    {
+      clean = "nh clean all";
+      update = "env -C ~/.config/nixos nix flake update";
+      upgrade = "nh os switch --update";
+      rebuild = "nh os switch";
+      rebuild-server = build + "server";
+      rebuild-nova = build + "nova";
+    };
 }
