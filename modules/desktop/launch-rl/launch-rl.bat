efc7698a7706f445825601608641986dea1206e4
Henry-Hiles <henry@henryhiles.com>
2025-10-17 16:43:28 -0400

    dont echo

--- modules/desktop/launch-rl/launch-rl.bat
+++ modules/desktop/launch-rl/launch-rl.bat
@@ -2,11 +2,9 @@
 
 set RL_PATH="Z:\home\quadradical\Documents\Games\rocketleague\Binaries\Win64"
 
-echo Launching BakkesMod...
 C:
 cd "C:\Program Files\BakkesMod"
 start BakkesMod.exe
-echo BakkesMod started, starting Rocket League
 Z:
 cd %RL_PATH%
 RocketLeague.exe %*
