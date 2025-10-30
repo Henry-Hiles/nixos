bc8f728059e8aed4040f17fc4942fa18203e669d
Henry-Hiles <henry@henryhiles.com>
2025-10-30 17:52:54 -0400

    add section

--- lib/status.nix
+++ lib/status.nix
@@ -1,8 +1,35 @@
 { pkgs, lib, ... }:
 services:
+
+#  {
+#       "collapsed": false,
+#       "gridPos": {
+#         "h": 1,
+#         "w": 24,
+#         "x": 0,
+#         "y": 0
+#       },
+#       "id": 261,
+#       "panels": [],
+#       "title": "Quick CPU / Mem / Disk",
+#       "type": "row"
+#     },
 pkgs.writers.writeJSON "status.json" {
   title = "Service Status";
-  panels = lib.imap0 (
+  panels = [
+    {
+      collapsed = false;
+      title = "Federated Nexus Service Statuses";
+      type = "row";
+      gridPos = {
+        h = 1;
+        w = 24;
+        x = 0;
+        y = 0;
+      };
+    }
+  ]
+  ++ (lib.imap0 (
     index:
     { name, service }:
     {
@@ -12,7 +39,7 @@ pkgs.writers.writeJSON "status.json" {
         h = 3;
         w = 4;
         x = index * w;
-        y = index * h;
+        y = (index * h) + 8;
       };
       datasource = {
         type = "prometheus";
@@ -52,5 +79,5 @@ pkgs.writers.writeJSON "status.json" {
         }
       ];
     }
-  ) services;
+  ) services);
 }
