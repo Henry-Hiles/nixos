086b008d84042b6aa47113887054dc757d3cb051
Henry-Hiles <henry@henryhiles.com>
2026-02-10 14:04:04 -0500

    remove useless line thats confusing

--- clients/quadraticserver/matrix/call.nix
+++ clients/quadraticserver/matrix/call.nix
@@ -13,46 +13,43 @@
   services =
     let
       domain = "call.federated.nexus";
+      livekitDomain = "livekit.call.federated.nexus";
+      lkJwtServiceDomain = "lk-jwt.call.federated.nexus";
     in
     {
       livekit = {
         enable = true;
         keyFile = config.age.secrets."livekitKeys.age".path;
+        settings.room.auto_create = false;
       };
 
       lk-jwt-service = {
         enable = true;
-        livekitUrl = "wss://${domain}/livekit/sfu";
+        livekitUrl = "wss://${livekitDomain}";
         keyFile = config.services.livekit.keyFile;
       };
 
-      caddy.virtualHosts."${domain}".extraConfig = ''
-        root * ${pkgs.element-call}
-        route {
-        	respond /config.json `${
-           builtins.toJSON {
-             default_server_config = {
-               "m.homeserver" = {
-                 "base_url" = config.services.matrix-continuwuity.settings.global.well_known.client;
-                 "server_name" = config.quad.matrix.domain;
+      caddy.virtualHosts = {
+        "${livekitDomain}".extraConfig = "reverse_proxy 127.0.0.1:7880";
+        "${lkJwtServiceDomain}".extraConfig = "reverse_proxy 127.0.0.1:8080";
+        "${domain}".extraConfig = ''
+          root * ${pkgs.element-call}
+          route {
+          	respond /config.json `${
+             builtins.toJSON {
+               default_server_config = {
+                 "m.homeserver" = {
+                   "base_url" = config.services.matrix-continuwuity.settings.global.well_known.client;
+                   "server_name" = config.quad.matrix.domain;
+                 };
                };
-             };
-             livekit.livekit_service_url = "https://${domain}/livekit";
-           }
-         }` 200
+             }
+           }` 200
 
-        	handle /livekit/sfu/get {
-        	  uri strip_prefix /livekit
-        	  reverse_proxy 127.0.0.1:8080
-        	}
-
-        	handle_path /livekit/sfu* {
-        	  reverse_proxy 127.0.0.1:7880
-        	}
-
-        	try_files {path} {path}/ /index.html
-        	file_server
-        }
-      '';
+            try_files {path} {path}/ /index.html
+          	file_server
+          }
+        '';
+      };
     };
 }
