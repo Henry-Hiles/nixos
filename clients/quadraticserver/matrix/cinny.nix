450fb914d7cc3b2cc11ed8245c954a0c1a2ed082
Henry-Hiles <henry@henryhiles.com>
2026-02-19 13:51:47 -0500

    update cinny

--- clients/quadraticserver/matrix/cinny.nix
+++ clients/quadraticserver/matrix/cinny.nix
@@ -13,12 +13,12 @@
             owner = "lapingvino";
             repo = "cinny";
             rev = "719381934efbe3aab098d4f077ec4ee14a30eddb";
-            hash = "";
+            hash = "sha256-qEUWTU3nCCqvMnC4JPSe2RiJURd6oFJqBHGHs5VNfZ4=";
           };
           npmDeps = pkgs.fetchNpmDeps {
             inherit src;
             name = "${old.pname}-${old.version}-npm-deps";
-            hash = "";
+            hash = "sha256-a4cnxo5smN+a6DWKPPkbGkd8gcQe/jazSEmrqKcN0fA=";
           };
         });
       }
