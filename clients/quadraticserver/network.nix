00325c241d86fb0efe20ce62f30edffa758b8c17
Henry-Hiles <henry@henryhiles.com>
2026-03-21 22:55:45 -0400

    nest network

--- clients/quadraticserver/network.nix
+++ clients/quadraticserver/network.nix
@@ -1,19 +1,21 @@
 {
   networking.useDHCP = false;
-  systemd.network.enable = true;
-  systemd.network.networks."30-wan" = {
-    matchConfig.Name = "enp1s0";
-    networkConfig.DHCP = "no";
-    address = [
-      "91.99.155.129/32"
-      "2a01:4f8:c012:d202::1/64"
-    ];
-    routes = [
-      {
-        Gateway = "172.31.1.1";
-        GatewayOnLink = true;
-      }
-      { Gateway = "fe80::1"; }
-    ];
+  systemd.network = {
+    enable = true;
+    networks."30-wan" = {
+      matchConfig.Name = "enp1s0";
+      networkConfig.DHCP = "no";
+      address = [
+        "91.99.155.129/32"
+        "2a01:4f8:c012:d202::1/64"
+      ];
+      routes = [
+        {
+          Gateway = "172.31.1.1";
+          GatewayOnLink = true;
+        }
+        { Gateway = "fe80::1"; }
+      ];
+    };
   };
 }
