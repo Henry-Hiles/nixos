dcad5d0e51c5bd858be224f0379035ec4962a49f
Henry-Hiles <henry@henryhiles.com>
2026-03-21 23:21:34 -0400

    Assign 64 IPv6 addresses to server, for future rotation

--- clients/quadraticserver/network.nix
+++ clients/quadraticserver/network.nix
@@ -4,15 +4,14 @@
     enable = true;
     networks."30-wan" = {
       matchConfig.Name = "enp1s0";
-      networkConfig = {
-        DHCP = "no";
-        IPv6PrivacyExtensions = true;
-      };
+      networkConfig.DHCP = "no";
 
       address = [
         "91.99.155.129/32"
-        "2a01:4f8:c012:d202::1/64"
-      ];
+      ]
+      # Assign 64 IPv6 addresses
+      ++ builtins.genList (i: "2a01:4f8:c012:d202::${builtins.toString (i + 1)}/64") 64;
+
       routes = [
         {
           Gateway = "172.31.1.1";
