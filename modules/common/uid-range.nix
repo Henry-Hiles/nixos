b4aeedd5b75b825850570576d8aaa0fac3d2f11d
Henry-Hiles <henry@henryhiles.com>
2026-06-30 12:49:09 -0400

    enable cgroups

--- modules/common/uid-range.nix
+++ modules/common/uid-range.nix
@@ -2,8 +2,10 @@
   nix.settings = {
     experimental-features = [
       "auto-allocate-uids"
+      "cgroups"
     ];
     auto-allocate-uids = true;
     system-features = [ "uid-range" ];
+    use-cgroups = true;
   };
 }
