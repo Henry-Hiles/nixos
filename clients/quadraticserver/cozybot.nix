01283469898a2a5d165ecb5d9699eb62e24038b3
Henry-Hiles <henry@henryhiles.com>
2026-06-30 16:21:21 -0400

    adjust bot protection on cozybot

--- clients/quadraticserver/cozybot.nix
+++ clients/quadraticserver/cozybot.nix
@@ -4,7 +4,19 @@
 
   services = {
     cozybot.enable = true;
-    caddy.virtualHosts."cozyp.federated.nexus".extraConfig =
-      "reverse_proxy unix//var/run/cozybot/socket";
+    caddy.virtualHosts."cozyp.federated.nexus".extraConfig = ''
+      respond /robots.txt <<EOF
+        User-agent: *
+        Disallow: *
+        EOF 200
+
+      defender redirect {
+        ranges aliyun aws deepseek githubcopilot gcloud oci azurepubliccloud openai mistral vultr cloudflare digitalocean linode
+        url https://ipv4.games/claim?name=federated.nexus
+      }
+
+      reverse_proxy unix//var/run/cozybot/socket
+    '';
+
   };
 }
