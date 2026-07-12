4767c6e4bf186535c576ad0add16e1f48d79cf43
Henry-Hiles <henry@henryhiles.com>
2026-07-12 13:12:02 -0400

    bump

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sable = {
-      url = "github:sableclient/sable/v1.19.3";
+      url = "github:sableclient/sable/v1.19.4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     redlib = {
