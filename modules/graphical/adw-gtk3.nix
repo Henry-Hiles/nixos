75c21e3d3c6629805df1a23fe479a6d8a441fc8f
Henry-Hiles <henry@henryhiles.com>
2026-07-13 02:09:16 +0000

    use adw gtk3 dark

--- modules/graphical/adw-gtk3.nix
+++ modules/graphical/adw-gtk3.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.adw-gtk3 ];
-  users.users.quadradical.maid.gsettings.settings.org.gnome.desktop.interface.gtk-theme = "adw-gtk3";
+  users.users.quadradical.maid.gsettings.settings.org.gnome.desktop.interface.gtk-theme =
+    "Adw-gtk3-dark";
 }
