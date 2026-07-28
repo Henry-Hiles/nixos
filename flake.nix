76e5061cb555966f6923a00745723d9377c65d51
Henry-Hiles <henry@henryhiles.com>
2026-07-27 20:22:16 -0400

    bump c10y

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nix-maid.url = "github:viperML/nix-maid";
     continuwuity = {
-      url = "git+https://forgejo.ellis.link/continuwuation/continuwuity?ref=ginger/fix-user-list-counting-puppets";
+      url = "git+https://forgejo.ellis.link/continuwuation/continuwuity";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
