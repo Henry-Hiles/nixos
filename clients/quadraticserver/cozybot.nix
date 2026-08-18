879bff808ac17aca123154826e5f95391e51bb21
Henry-Hiles <henry@henryhiles.com>
2026-08-18 17:00:14 -0400

    remove defender

--- clients/quadraticserver/cozybot.nix
+++ clients/quadraticserver/cozybot.nix
@@ -10,11 +10,6 @@
         Disallow: *
         EOF 200
 
-      defender redirect {
-        ranges aliyun aws deepseek githubcopilot gcloud oci azurepubliccloud openai mistral vultr cloudflare digitalocean linode
-        url https://ipv4.games/claim?name=federated.nexus
-      }
-
       reverse_proxy unix//var/run/cozybot/socket
     '';
 
