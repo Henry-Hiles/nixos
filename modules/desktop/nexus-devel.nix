16ea95a981fa47f4c52ec8cd45664c3b019b614f
Henry-Hiles <henry@henryhiles.com>
2026-08-25 14:43:22 -0400

    add nexus-devel

--- modules/desktop/nexus-devel.nix
+++ modules/desktop/nexus-devel.nix
@@ -3,4 +3,13 @@
   systemd.packages = [
     pkgs.kdePackages.kunifiedpush
   ];
+
+  networking.firewall = {
+    allowedUDPPorts = [
+      8008
+    ];
+    allowedTCPPorts = [
+      8008
+    ];
+  };
 }
