3f8c31b43673fa4744d1ca219aef08ae95cfaaef
Henry-Hiles <henry@henryhiles.com>
2026-09-07 13:09:16 -0400

    bump deps

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sable = {
-      url = "github:sableclient/sable/v1.20.0";
+      url = "github:sableclient/sable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
