1aadf2a468dbd4f486d3df1de1c17d7ffc774f47
Henry-Hiles <henry@henryhiles.com>
2026-09-23 15:43:48 -0400

    run media off of sshfs for continuwuity

--- clients/quadraticserver/matrix/continuwuity.nix
+++ clients/quadraticserver/matrix/continuwuity.nix
@@ -1,10 +1,13 @@
 { lib, pkgs, ... }: {
-  systemd.services.continuwuity.serviceConfig = {
-    MemoryMax = "5.5G";
-    MemorySwapMax = "4.5G";
-    TimeoutStartSec = "10m";
-    Restart = lib.mkForce "always";
-    ExecStartPost = "/bin/sh -c 'until ${lib.getExe pkgs.curl} -s -f https://matrix.federated.nexus/.well-known/matrix/client; do sleep 1; done'";
+  systemd.services.continuwuity = {
+    unitConfig.RequiresMountsFor = [ "/var/lib/private/continuwuity/media" ];
+    serviceConfig = {
+      MemoryMax = "5.5G";
+      MemorySwapMax = "4.5G";
+      TimeoutStartSec = "10m";
+      Restart = lib.mkForce "always";
+      ExecStartPost = "/bin/sh -c 'until ${lib.getExe pkgs.curl} -s -f https://matrix.federated.nexus/.well-known/matrix/client; do sleep 1; done'";
+    };
   };
 
   quad.matrix = rec {
@@ -27,4 +30,23 @@
       };
     };
   };
+
+  system.fsPackages = [
+    pkgs.sshfs
+  ];
+
+  systemd.mounts = [
+    {
+      what = "u675579@u675579.your-storagebox.de:/media";
+      where = "/var/lib/private/continuwuity/media";
+      type = "fuse.sshfs";
+      options = builtins.concatStringsSep "," [
+        "_netdev"
+        "ServerAliveInterval=15"
+        "ServerAliveCountMax=3"
+        "IdentityFile=/home/quadradical/.ssh/id_ed25519"
+        "allow_other"
+      ];
+    }
+  ];
 }
